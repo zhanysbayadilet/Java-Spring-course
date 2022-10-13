fe2c55fd0163302d2f27616cfdd85e596c73680b
Adilet Zhanysbay <adiletzhan02@gmail.com>
2022-10-13 19:45:34 +0600

    first commit

---
+++ src/main/java/com/javaspring/javaspringwebapp/JavaSpringWebappApplication.java
@@ -0,0 +1,13 @@
+package com.javaspring.javaspringwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaspring/javaspringwebapp/JavaSpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaspring.javaspringwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
